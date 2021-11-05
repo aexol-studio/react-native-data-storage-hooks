31ab3b628c577578772374c9de23d455f0923911
MPuzio15 <martynapuzio93@gmail.com>
2021-11-05 10:13:00 +0100

    fix

--- dist/module/hooks/index.js
+++ dist/module/hooks/index.js
@@ -2,5 +2,5 @@ export * from './useGetFromStorage.hooks';
 export * from './useGetWholeStorage.hooks';
 export * from './useSetMultipleValues.hook';
 export * from './useSetSingleValue.hook';
-export * from './useDeleteFromStorage.hooks';
+export * from './useDeleteFromStorage.hooks'; //comment
 //# sourceMappingURL=index.js.map
